3aebdc6fbab09ddcc57ab47fa61efbcbdaebe530
hawkes <douglas.hawkes@gmail.com>
2017-05-27 18:20:55 -0600

    working on login service and database classes

--- server/src/main/java/doughawkes/fmserver/dataAccess/Database.java
+++ server/src/main/java/doughawkes/fmserver/dataAccess/Database.java
@@ -16,27 +16,28 @@ public class Database {
     private PersonDao personDao;
     private UserDao userDao;
 
+    private boolean allTransactionsSucceeded;
+
     /**
      * Constructor for the Database class as a container for instantiated specific Dao objects
      */
     public Database() {
+        allTransactionsSucceeded = true;
 
     }
 
-// this is being loaded in the Server main method.
-/*    public boolean startDriver(String db) {
-        return false;
-    }*/
-
     public void startTransaction() {
-        String dbName = "server" + File.separator + "db" + File.separator + "familyMap.sqlite";
+        String dbName = "server/db/familyMap.sqlite";
         String connectionURL = "jdbc:sqlite:" + dbName;
         connection = null;
         try {
             // Open a database connection
             connection = DriverManager.getConnection(connectionURL);
-            // Start a transaction
-            //connection.setAutoCommit(false);
+            System.out.println("Database opened successfully");
+            // Start a transaction (this is how it's done)
+            // Not sure if I want the transaction to start here or in each dao.
+            // I think I want to have each database either commit or rollback all changes.
+            connection.setAutoCommit(false);
         }
         catch (SQLException e) {
             System.out.println("database driver issue");
@@ -45,7 +46,27 @@ public class Database {
     }
 
     public boolean endTransaction() {
-        return false;  // THIS IS ONLY A FILLER, CHANGE IT, DEPENDING.
+        try {
+            if (allTransactionsSucceeded) {
+                connection.commit();
+            }
+            else {
+                connection.rollback();
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Could neither commit nor rollback connection!");
+        }
+        finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        connection = null;
+        return allTransactionsSucceeded;  // THIS IS ONLY A FILLER, CHANGE IT, DEPENDING.
     }
 
     public AuthTokenDao getAuthTokenDao() {
