bea9eb2d94fea689ec15c78c6c96cc8aff1b92f2
hawkes <douglas.hawkes@gmail.com>
2017-06-19 09:23:23 -0600

    Filter layout works but is not properly wired up. Setting widgets persist. going to work on filter now. Map types working.
    Fixed: crash from clicking info window before marker selected.
    Fixed: Weird crash when clicking on an event from personactivity when it belongs to root user.
    
    PersonActivity works fully but is missing icons
    
    Oooh I can click on events from the PersonActivity now :) Need to change the menu to blank except the back button.
    
    Family lines ARE working even thought I thought they weren't
    Spouse line draws to appropriate earliest event in spouse life
    
    Life story Lines are drawing with changes in thickness
    Filters confirmed working but only programmed.
    Filters are programmed in but FilterActivity has no layout nor is it wired up.
    Fixed issue with the user's Person not having the same personID in the User table of the database and the Person table of the database.
    
    SettingsActivity fully wired up including toggles. Now the map needs to draw lines according to the settings.
    I might work on the FilterActivity next.
    
    In the middle of making the Settings Spinners work. Making a Settings class in Model.
    
    SettingsActivity Re-Sync data works.
    
    Menu buttons open their respective activities, PersonActivity and MapActivity bare bones. Need to make layouts for all but SettingsActivity
    
    SettingsActivity layout is perfect (for my phone!)
    SettingsActivity implements nothing and doesn't use a fragment.
    
    Map menu is able to check for the activity (mainactivity or mapactivity) and change accoringly.
    The other activities will need the up (go to top) button, but preserve settings if possible.
    
    Toolbar/Actionbar action buttons visible. Now need to make them clickable and go to the respective fragments.
    
    Info panel on map Fragment appears. Need to fill it with the right stuff and make the icon dynamic

--- app/src/main/java/hawkes/fmc/ui/FilterActivity.java
+++ app/src/main/java/hawkes/fmc/ui/FilterActivity.java
@@ -1,15 +1,158 @@
 package hawkes.fmc.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import hawkes.fmc.R;
+import hawkes.fmc.model.Filter;
+import hawkes.fmc.model.Model;
 
 public class FilterActivity extends AppCompatActivity {
+
+    RecyclerView mEventsRecyclerView;
+
+    LinearLayoutManager mEventsLinearLayoutManager;
+
+    ItemAdapter mEventsTestAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        // Get Recycler View by id from layout file
+        mEventsRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_filters);
+
+        // Create Linear Layout Manager which defines how it will be shown on the screen
+        mEventsLinearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mEventsLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        // Set Layout Manager in the RecyclerView
+        mEventsRecyclerView.setLayoutManager(mEventsLinearLayoutManager);
+
+        // Create Adapter object from the data by calling default Constructor
+        mEventsTestAdapter = new ItemAdapter(getFilters());
+
+        // Set RecyclerView Adapter
+        mEventsRecyclerView.setAdapter(mEventsTestAdapter);
+
+        // Done! Hooray !!
+    }
+
+    private class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ListItemViewHolder> {
+
+        private ArrayList<Filter> filtersList;
+
+        public ItemAdapter(ArrayList<Filter> filtersList) {
+            this.filtersList = filtersList;
+
+        }
+
+        @Override
+        public ListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            final View itemView = LayoutInflater.from(
+                    parent.getContext()).inflate(R.layout.filter_recyclerview_viewholder, parent, false);
+
+            return new ListItemViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ListItemViewHolder holder, int position) {
+            Filter filter = filtersList.get(position);
+            String topText = filter.getFilterType();
+            holder.topText.setText(topText);
+
+            holder.bottomText.setText("Filter by " + filter.getFilterType());
+
+        }
+
+        /**
+         * Returns the total number of items in the data set held by the adapter.
+         *
+         * @return The total number of items in this adapter.
+         */
+        @Override
+        public int getItemCount() {
+            return filtersList.size();
+        }
+
+        public class ListItemViewHolder extends RecyclerView.ViewHolder implements CompoundButton.OnCheckedChangeListener {
+            //public IconTextView mIcon;
+            public TextView topText;
+            public TextView bottomText;
+            public Switch filterSwitch;
+
+            public ListItemViewHolder(View itemView) {
+                super(itemView);
+
+                //mIcon = (ImageView) itemView.findViewById(R.id.child_list_item_icon);
+                topText = (TextView) itemView.findViewById(R.id.filterTitle);
+                bottomText = (TextView) itemView.findViewById(R.id.filterSubtitle);
+
+                filterSwitch = (Switch) itemView.findViewById(R.id.filterSwitch);
+
+                //itemView.setOnClickListener(this);
+                filterSwitch.setOnCheckedChangeListener(this);
+
+            }
+
+            /**
+             * Called when a view has been clicked.
+             *
+             * @param v The view that was clicked.
+             */
+//            @Override
+//            public void onClick(View v) {
+//                Filter clickedFilter = filtersList.get(getAdapterPosition());
+//                Model model = Model.getModel();
+//                model.getFilters().get(clickedFilter.getFilterType()).setOn(); // is this event sufficient or is it out of context?
+//
+//                Intent intent = new Intent(PersonActivity.this, MapActivity.class);
+//                //intent.putExtra("personOfInterest", model.getPersons().get(selectedEvent.getPersonID()));
+//                startActivity(intent);
+//            }
+
+            /**
+             * Called when the checked state of a compound button has changed.
+             *
+             * @param buttonView The compound button view whose state has changed.
+             * @param isChecked  The new checked state of buttonView.
+             */
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Filter clickedFilter = filtersList.get(getAdapterPosition());
+                Model model = Model.getModel();
+                model.getFilters().get(clickedFilter.getFilterType()).setOn(isChecked);
+                //filtersChanged();
+            }
+
+//            public void bind(ListChild listChild) {
+//                mTopText.setText(listChild.getTopText());
+//                mBottomText.setText(listChild.getBottomText());
+//            }
+        }
+    }
+
+    private ArrayList<Filter> getFilters() {
+
+        Model model = Model.getModel();
+
+        ArrayList<Filter> filtersInOrder = new ArrayList<>();
+        for (Filter f : model.getFilters().values()) {
+            filtersInOrder.add(f);
+        }
+        return filtersInOrder;
     }
 }
